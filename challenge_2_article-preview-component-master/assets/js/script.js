a9464cdca138d2421e82365ab8d9e5fc81857cb7
Jose Ruivo <jose.ruivo@elecnor.es>
2020-10-06 09:49:50 +0100

    Fix positioning of the widget popup

--- challenge_2_article-preview-component-master/assets/js/script.js
+++ challenge_2_article-preview-component-master/assets/js/script.js
@@ -1,22 +1,23 @@
-let shareBtn = document.querySelector('.share-btn');
-let popup = document.querySelector('.popup');
-
-
-console.log(shareBtn);
-console.log(popup);
-
+let shareBtn = document.querySelector(".share-btn");
+let popup = document.querySelector(".popup");
 
 let isPopupOpen = false;
 
-shareBtn.addEventListener('click', () => {
+shareBtn.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(e.currentTarget);
 
-
+  if (
+    (e.target.parentElement === shareBtn && e.target !== popup) ||
+    e.target === e.currentTarget
+  ) {
     if (isPopupOpen) {
-        popup.classList.add('hide');
+      popup.classList.add("hide");
+      shareBtn.classList.remove("active");
     } else {
-        popup.classList.remove('hide');
+      popup.classList.remove("hide");
+      shareBtn.classList.add("active");
     }
-    isPopupOpen=!isPopupOpen;
-})
-
-
+    isPopupOpen = !isPopupOpen;
+  }
+});
